8ef8e84d9f48e188a3ad8f91b293f88af17639f1
bimmbim <bimabaraja76@gmail.com>
2025-05-05 11:41:21 +0700

    membuat dan meanggil object nilai pelajar

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -6,3 +6,23 @@ class manusia{
     public:
         void showNilaipelajar(pelajar &x);
 };
+
+class pelajar{
+    private:
+        int nilai;
+
+    public:
+        pelajar(){nilai = 100;}
+        friend void manusia::showNilaipelajar(pelajar &x);
+};
+
+void manusia::showNilaipelajar(pelajar &x){
+    cout << x.nilai;
+}
+
+int main(){
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaipelajar(pbudi);
+    return 0;
+}
